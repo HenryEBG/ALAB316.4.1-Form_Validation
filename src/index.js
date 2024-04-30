a2dd6178defef1f39962cde6992793437b9cf124
HenryEBG <berganza386@gmail.com>
2024-04-30 17:29:23 -0400

    adding email validation

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const registerForm = document.querySelector("#registration")
 
 
 const userName = registerForm.elements["username"]
+const email = registerForm.elements["email"]
 
 function validateUsername() {
   const twoOrMorePattern = /(.)(?<!1.)(?!.*\1)(.)(?<!3)/
@@ -28,15 +29,31 @@ function validateUsername() {
   return userName.value
 }
 
+function validateEmail(){
+  const validEmailPattern = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/
+  const notValidEmailExample = /@example.com/
+  if (email.value.match(validEmailPattern) === null) {
+    window.alert("The email must be a valid email address.")
+    email.focus()
+    return false
+  } else if (email.value.match(notValidEmailExample) !== null) {
+    window.alert(`The email must not be from the domain "example.com."`)
+    email.focus()
+    return false
+  }
+}
+
 
 function validateRegister(event) {
 
   event.preventDefault()
-  const valUserName = validateUsername()
-  if (valUserName === false) {
+  if (validateUsername() === false) {
     event.returnValue = false;
     return false;
-  }
+  } else if(validateEmail()===false){
+    event.returnValue = false;
+    return false;
+  } 
 
 
 }
